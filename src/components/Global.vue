37cd9fe13a57559a2536dcffb3f0e5914256c961
1035818889 <1035818889@qq.com>
2019-12-22 16:20:07 +0800

    提交

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -1,10 +1,10 @@
 ﻿<script>
-    //const serverSrc='http://19.131.100.156:8080/mvc';
-    //const serverSrcRoot='http://19.131.100.156:8080';
-    //const serverSrcPdfView='http://19.131.100.156:8080/pdfjs/web/viewer.html?file=';
-    const serverSrc='http://sscz.zhaoqingglx.cn/mvc';
-    const serverSrcRoot='http://sscz.zhaoqingglx.cn';
-    const serverSrcPdfView='http://sscz.zhaoqingglx.cn/pdfjs/web/viewer.html?file=';
+    const serverSrc='http://19.131.100.156:8080/mvc';
+    const serverSrcRoot='http://19.131.100.156:8080';
+    const serverSrcPdfView='http://19.131.100.156:8080/pdfjs/web/viewer.html?file=';
+    //const serverSrc='http://sscz.zhaoqingglx.cn/mvc';
+    //const serverSrcRoot='http://sscz.zhaoqingglx.cn';
+    //const serverSrcPdfView='http://sscz.zhaoqingglx.cn/pdfjs/web/viewer.html?file=';
     export default {
         name: "Global.vue",
         serverSrc,
